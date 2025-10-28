c98ae8e7ecadafebd12aaa400e31ff6464b092f9
bdb150305-pixel <bdb150305@gmail.com>
2025-10-28 23:53:27 +0700

    Thêm dong

--- sort.cpp
+++ sort.cpp
@@ -108,10 +108,11 @@ void Mergesort(int a[],int low,int high,int n){
 
 int main(){
     int a[10] ={6,4,8,3,5,2,7,1,9,0};
-    //Bubblesort(a,10);
-    //Selectionsort(a,10);
-    //Insertionsort(a,10);
-    //Quicksort(a,0,9,10);
+    Bubblesort(a,10);
+    Selectionsort(a,10);
+    Insertionsort(a,10);
+    Quicksort(a,0,9,10);
     Mergesort(a,0,9,10);
     cout << "END";
+    cout << "/nOk con dê";
 }
